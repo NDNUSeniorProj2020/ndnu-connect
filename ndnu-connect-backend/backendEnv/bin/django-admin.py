80da8e284fcdf6e8168ffcc91284408c3b149354
digitaluniverse <contact@mattdaniele.com>
2020-02-18 11:36:06 -0800

    removed localEnv

--- ndnu-connect-backend/backendEnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/matthewdaniele/SeniorProject/project/ndnu-connect/ndnu-connect-backend/backendEnv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
