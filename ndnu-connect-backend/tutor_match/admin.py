c048e2c46c2d286b64b84ede587027a4e81b9ab9
markFalcone <falconemark16@yahoo.com>
2020-02-18 19:10:21 -0800

    Added and registerd models to admin

--- ndnu-connect-backend/tutor_match/admin.py
+++ ndnu-connect-backend/tutor_match/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
 from .models import Tutor
+from .models import Student
+from .models import Department
+from .models import Subject
+from .models import Schedule
+from .models import SubjToDept
+from .models import TuitionLocation
+from .models import TuitionMethod
 
 # Register your models here.
 admin.site.register(Tutor)
+admin.site.register(Student)
+admin.site.register(Department)
+admin.site.register(Subject)
+admin.site.register(Schedule)
+admin.site.register(SubjToDept)
+admin.site.register(TuitionLocation)
+admin.site.register(TuitionMethod)
