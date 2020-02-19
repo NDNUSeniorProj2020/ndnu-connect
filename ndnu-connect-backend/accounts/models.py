ea3d6765f73b3e5df82fbc4bdbc886048fb31d28
pv-gitjob <praveenvandeyar@gmail.com>
2020-02-18 20:52:20 -0800

    update account model

--- ndnu-connect-backend/accounts/models.py
+++ ndnu-connect-backend/accounts/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class person(models.Model):
-
-    person_name = models.CharField(max_length=20)
-    email = models.EmailField()
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    #alumni
 
     def __str__(self):
-        return self.name
+        return self.user.name
+
+
+
+
