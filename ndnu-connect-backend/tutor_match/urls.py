4a09d8f45545c2dc78589eec34452a8de4a96c8c
digitaluniverse <contact@mattdaniele.com>
2020-02-18 19:36:30 -0800

    Fixed JWT Error and added tutor_match urls.py to main urls.py

--- ndnu-connect-backend/tutor_match/urls.py
+++ ndnu-connect-backend/tutor_match/urls.py
@@ -3,13 +3,13 @@ from rest_framework import routers
 from tutor_match import views
 
 router = routers.DefaultRouter()
-router.register(r'api/department', views.DepartmentViewSet)
-router.register(r'api/subject', views.SubjectViewSet)
-router.register(r'api/schedule', views.ScheduleViewSet)
+router.register(r'department', views.DepartmentViewSet)
+router.register(r'subject', views.SubjectViewSet)
+router.register(r'schedule', views.ScheduleViewSet)
 # router.register(r'/api/tuitionmethod', views.TuitionMethodSerializer)
 # router.register(r'/api/tuitionlocation', views.TuitionLocationSerializer)
-router.register(r'api/tutor', views.TutorViewSet)
-router.register(r'api/student', views.StudentViewSet)
+router.register(r'tutor', views.TutorViewSet)
+router.register(r'student', views.StudentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
