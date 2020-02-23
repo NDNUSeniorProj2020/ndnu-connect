59b40ffaf664df254385ac151cae0f230bd958f8
pv-gitjob <praveenvandeyar@gmail.com>
2020-02-22 22:25:15 -0800

    added blank=true for person fields

--- ndnu-connect-backend/accounts/models.py
+++ ndnu-connect-backend/accounts/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Person(AbstractUser):
-    graduated = models.IntegerField(null=True)
-    major = models.CharField(max_length=20, null=True)
-    company = models.CharField(max_length=20, null=True)
-    job_title = models.CharField(max_length=20, null=True)
-    about = models.TextField(null=True)
+    graduated = models.IntegerField(null=True, blank=True)
+    major = models.CharField(max_length=20, null=True, blank=True)
+    company = models.CharField(max_length=20, null=True, blank=True)
+    job_title = models.CharField(max_length=20, null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.get_full_name()
