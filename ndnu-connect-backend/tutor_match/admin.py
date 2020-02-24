252eea690c5daf8c1018a3a52df938bf812ed355
markFalcone <falconemark16@yahoo.com>
2020-02-23 17:05:33 -0800

    added the admin page and python script to add fake data. TUTOR AND PERSON STILL NEED TO BE UPDATED

--- ndnu-connect-backend/tutor_match/admin.py
+++ ndnu-connect-backend/tutor_match/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+#from django.contrib.auth.models import Group
 from .models import Tutor
 from .models import Student
 from .models import Department
@@ -9,9 +9,13 @@ from .models import SubjToDept
 
 
 # Register your models here.
+admin.site.site_header = "NDNU Conect Admin Page" #title
+#admin.site.unregister(Group) #not displaying the Group
 admin.site.register(Tutor)
 admin.site.register(Student)
 admin.site.register(Department)
 admin.site.register(Subject)
 admin.site.register(Schedule)
 admin.site.register(SubjToDept)
+
+# chainging how the tutor looks
