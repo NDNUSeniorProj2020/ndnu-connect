265e1de40f7da35b2bdd5a08a5cf6a0cc7d350bc
digitaluniverse <contact@mattdaniele.com>
2020-02-25 19:49:35 -0800

    added jobs

--- ndnu-connect-backend/job_find/models.py
+++ ndnu-connect-backend/job_find/models.py
@@ -3,23 +3,20 @@ from django.utils import timezone
 
 
 class Job(models.Model):
-    title = models.TextField(max_length=50, blank=true)
-    description = models.TextField(max_length=250, blank=true)
-    qualifications = models.TextField(max_length=250, blank=true)
+    title = models.TextField(max_length=50, blank=True)
+    description = models.TextField(max_length=250, blank=True)
+    qualifications = models.TextField(max_length=250, blank=True)
+    pay = models.TextField(max_length=115, blank=True)
+    link = models.TextField(max_length=115, blank=True)
+    date = models.DateTimeField(default=timezone.now)
 
     class JobTypes(models.TextChoices):
         FullTime = 'FULL', 'Full Time'
         PartTime = 'PART', 'Part Time'
         Intern = 'INTR', 'Internship'
 
-        def __str__(self):
-
-    return self.pay
-
     type = models.CharField(choices=JobTypes.choices, max_length=4, null=True)
-    pay = models.TextField(max_length=115, blank=true)
-    link = models.TextField(max_length=115, blank=true)
-    date = models.DateTimeField(default=timezone.now)
+
 
     def __str__(self):
         return self.link
