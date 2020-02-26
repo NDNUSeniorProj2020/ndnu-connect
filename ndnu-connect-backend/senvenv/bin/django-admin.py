f6e3fb5a29a81d48159c7735390b6e90b68777db
digitaluniverse <contact@mattdaniele.com>
2020-02-25 19:59:13 -0800

    added jobs

--- ndnu-connect-backend/senvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/josealvarez/Documents/Senior Project/Ndnu Connect/ndnu-connect/ndnu-connect-backend/senvenv/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
