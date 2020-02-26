638e20fb1bd83da3dfd843813cac2a1f18ce80b2
markFalcone <falconemark16@yahoo.com>
2020-02-25 20:51:29 -0800

    corected the python script to add fake user data

--- ndnu-connect-backend/accounts/models.py
+++ ndnu-connect-backend/accounts/models.py
@@ -9,10 +9,12 @@ class Person(AbstractUser):
     job_title = models.CharField(max_length=20, null=True, blank=True)
     about = models.TextField(null=True, blank=True)
 
+    def create_user(self, username, password):
+        self.username=username
+        self.password=password
+        self.save()
+        return self
+
+
     def __str__(self):
         return self.username
-
-
-
-
-
