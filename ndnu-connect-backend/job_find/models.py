891aae7f6d160601208789e50aaeecf75cce31d6
Jose Alvarez Avina <j.alvarez22@live.com>
2020-02-25 19:25:15 -0800

    jobModels.py

--- ndnu-connect-backend/job_find/models.py
+++ ndnu-connect-backend/job_find/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+
+
+class Job(models.Model):
+    title = models.TextField(max_length=50, blank=true)
+    description = models.TextField(max_length=250, blank=true)
+    qualifications = models.TextField(max_length=250, blank=true)
+
+    class JobTypes(models.TextChoices):
+        FullTime = 'FULL', 'Full Time'
+        PartTime = 'PART', 'Part Time'
+        Intern = 'INTR', 'Internship'
+
+        def __str__(self):
+
+    return self.pay
+
+    type = models.CharField(choices=JobTypes.choices, max_length=4, null=True)
+    pay = models.TextField(max_length=115, blank=true)
+    link = models.TextField(max_length=115, blank=true)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.link
 
 # Create your models here.
