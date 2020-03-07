077295ae42b26b96259ed2175ca1739c05d638d0
Christian Lapinig <clapinig93@gmail.com>
2020-03-07 11:26:50 -0800

    Change authentication urls

--- ndnu-connect-backend/accounts/urls.py
+++ ndnu-connect-backend/accounts/urls.py
@@ -5,6 +5,6 @@ from .views import current_user, UserList, RegistrationAPIView, LoginAPIView
 urlpatterns = [
     path('current_user/', current_user),
     path('users/', UserList.as_view()),
-    path('create-user/', RegistrationAPIView.as_view()),
-    path('login-user/', LoginAPIView.as_view())
+    path('register/', RegistrationAPIView.as_view()),
+    path('login/', LoginAPIView.as_view())
 ]
