65f9dc95d17099d5ee5e505bc588a1d6dd37b954
Christian Lapinig <clapinig93@gmail.com>
2020-03-08 16:19:51 -0700

    Add active status on User admin page

--- ndnu-connect-backend/accounts/admin.py
+++ ndnu-connect-backend/accounts/admin.py
@@ -5,9 +5,9 @@ from .models import Person, User
 
 class UserAdmin(BaseUserAdmin):
     model = User
-    list_filter = ('is_staff',)
+    list_filter = ('is_staff', 'is_active',)
     ordering = ('email',)
-    list_display = ('email', 'first_name', 'last_name', 'phone_number', 'is_staff',)
+    list_display = ('email', 'first_name', 'last_name', 'phone_number', 'is_active', 'is_staff',)
     fieldsets = (
         (None, {'fields': ('email', 'first_name', 'last_name', 'phone_number', 'password')}),
         ('Permissions', {'fields': ('is_staff',)})
