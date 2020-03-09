0290816f6fbf553e02fb86bf85bd279311827c49
Christian Lapinig <clapinig93@gmail.com>
2020-03-08 22:38:12 -0700

    Add SessionAuthentication to bypass Authorization header

--- ndnu-connect-backend/ndnuconnect/settings.py
+++ ndnu-connect-backend/ndnuconnect/settings.py
@@ -140,8 +140,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
         'accounts.backends.JWTAuthentication',
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication'
     ),
     'EXCEPTION_HANDLER': 'core.exceptions.core_exception_handler',
     'NON_FIELD_ERRORS_KEY': 'error'
