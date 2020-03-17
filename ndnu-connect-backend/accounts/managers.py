b5ab45b4cdf8ff3407d7cff3c2166e488ae8ad7b
Christian Lapinig <clapinig93@gmail.com>
2020-03-17 14:57:57 -0700

    Fix db_populate script to use user fields when creating Job and Tutor instances

--- ndnu-connect-backend/accounts/managers.py
+++ ndnu-connect-backend/accounts/managers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password, display_name, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address.')
         if password is None:
@@ -10,14 +10,15 @@ class UserManager(BaseUserManager):
 
         user = self.model(
             email=self.normalize_email(email),
+            display_name=display_name,
             **extra_fields
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email, password, display_name, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, password, display_name, **extra_fields)
