fb1b5432360bf051d4f0ba30b4b3d286b9eec74d
pv-gitjob <praveenvandeyar@gmail.com>
2020-03-24 18:49:11 -0700

    Move urls from tutor-match to ndnu-connect.

--- ndnu-connect-backend/boards/urls.py
+++ ndnu-connect-backend/boards/urls.py
@@ -1,7 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
 from django.conf.urls import url
 from django.contrib import admin
 from .views import home, board_topics
+from boards import views
 
+router = routers.DefaultRouter()
+router.register(r'board', views.BoardViewSet)
+router.register(r'topic', views.TopicViewSet)
+router.register(r'post', views.PostViewSet)
 
 urlpatterns = [
     url('', home, name='boards'),
