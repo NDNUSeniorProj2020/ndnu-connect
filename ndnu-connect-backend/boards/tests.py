6486f43b45474c60b592745570d7f95c866b0539
pv-gitjob <praveenvandeyar@gmail.com>
2020-04-06 22:46:59 -0700

    Add board test

--- ndnu-connect-backend/boards/tests.py
+++ ndnu-connect-backend/boards/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Board, Topic, Post
 
-# Create your tests here.
+
+class BoardTestCase(TestCase):
+    def setUp(self):
+        board1 = Board(name='Events', description='Upcoming events at NDNU')
+        board1.save()
+        board2 = Board(name='Textbooks', description='Buy or sell textbooks')
+        board2.save()
+
+    def testBoardNames(self):
+        board1 = Board.objects.get(name="Events")
+        board2 = Board.objects.get(name="Textbooks")
+        self.assertEqual(board1.name, 'Events')
+        self.assertEqual(board2.name, 'Textbooks')
