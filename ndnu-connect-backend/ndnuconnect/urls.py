689904c6f56b3f5a3fa6cc3bbfb31546812a17a0
Raul Flores <raulflores650@gmail.com>
2020-04-17 15:52:40 -0700

    link to edit a job posting

--- ndnu-connect-backend/ndnuconnect/urls.py
+++ ndnu-connect-backend/ndnuconnect/urls.py
@@ -46,6 +46,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     
     path('api/', include(router.urls)),
-    path('api/job/<int:pk>/update/',jobviews.JobUpdate.as_view()),
+    path('api/job/<int:pk>/update/',jobviews.JobUpdateView.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
