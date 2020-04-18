671d14238933601dd7013bf92a6156124bed828b
Jonathon Chenvert <jchenvert1@gmail.com>
2020-04-18 14:59:27 -0700

    Updated Accounts API

--- ndnu-connect-backend/accounts/serializers.py
+++ ndnu-connect-backend/accounts/serializers.py
@@ -55,7 +55,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name', 'phone_number', 'password', 'token',)
+        fields = ('id', 'email', 'first_name', 'last_name', 'phone_number', 'password', 'token',)
         read_only_fields = ('token',)
 
     # Update user
